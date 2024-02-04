e15fc3dfb528bcb474c970f588a3ba5fcbffad6f
Richard Ocampo <richard.ocampo@georgebrown.ca>
2024-02-04 15:30:19 -0500

    Progress Bar

--- Assets/Scripts/GameUIStateMachine.cs
+++ Assets/Scripts/GameUIStateMachine.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameUIStateMachine : MonoBehaviour
 {
-    public GameObject playScreen, finishScreen, WinScreen, LoseScreen;
+    public GameObject playScreen, finishBar, WinScreen, LoseScreen;
 
 
 
@@ -16,25 +16,35 @@ public class GameUIStateMachine : MonoBehaviour
 
     public void ChangeToPlayScreen()
     {
-        
+        playScreen.SetActive(true);
+        LoseScreen.SetActive(false);
+        WinScreen.SetActive(false);
 
         
     }
 
     public void ChangeToFinishScreen()
     {
-       
+        finishBar.SetActive(true);
+        playScreen.SetActive(false);
+        LoseScreen.SetActive(false);
+        WinScreen.SetActive(false);
     }
 
     public void ChangeToWinScreen()
     {
-
-      
+        playScreen.SetActive(false);
+        LoseScreen.SetActive(false);
+        WinScreen.SetActive(true);
+        finishBar.SetActive(false);
     }
 
     public void ChangeToLoseScreen()
     {
-      
+        playScreen.SetActive(false);
+        LoseScreen.SetActive(true);
+        WinScreen.SetActive(false);
+        finishBar.SetActive(false);
     }
 
 
