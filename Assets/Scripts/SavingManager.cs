53dbb99c5f713ce24a37d7753e8ccac8abe452b6
Andrii Gastello <concof.zp@gmail.com>
2024-03-31 22:52:03 -0400

    Global Code Fix & Clean Up for Beta Release!

--- Assets/Scripts/SavingManager.cs
+++ Assets/Scripts/SavingManager.cs
@@ -1,90 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
-using UnityEngine.InputSystem.EnhancedTouch;
-
-[Serializable]
-public class SaveData
-{
-    public int level;
-    public int coins;
-    public int gem;
-    public int da;
-}
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
 public static class SavingManager
 {
-    public static void SaveGame(int level, int coins, int gem, int da)
+    public static void SaveGame(PlayerData playerData)
     {
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/MySaveData.dat"); 
-        SaveData data = new SaveData();
-        data.level = level;
-        data.coins = coins;
-        data.gem = gem;
-        data.da = da;
+        FileStream file = File.Create(Application.persistentDataPath + "/MySaveData.dat");
 
-        bf.Serialize(file, data);
+        bf.Serialize(file, playerData);
         file.Close();
-        Debug.Log("Game datasaved!");
+        Debug.Log("Game data saved!");
     }
 
-    static SaveData LoadGame()
+    public static PlayerData LoadGame()
     {
         if (File.Exists(Application.persistentDataPath + "/MySaveData.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/MySaveData.dat", FileMode.Open);
-            SaveData data = (SaveData) bf.Deserialize(file);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
             file.Close();
-            return data;
 
-            Debug.Log("Game dataloaded!");
+            Debug.Log("Game data loaded!");
+            return data;
         }
         else
+        {
             Debug.LogError("There is no save data!");
-
-        return null;
+            return null;
+        }
     }
 
-    public static int LoadLevel()
-    {
-        SaveData data = LoadGame();
-        return data.level;
-    }
-
-    public static int LoadCoins()
-    {
-        SaveData data = LoadGame();
-        return data.coins;
-    }
-
-    public static int LoadGem()
-    {
-        SaveData data = LoadGame();
-        return data.gem;
-    }
-
-    public static int LoadData()
-    {
-        SaveData data = LoadGame();
-        return data.da;
-    }
-
-
-    static void ResetData()
+    public static void ResetData()
     {
         if (File.Exists(Application.persistentDataPath + "/MySaveData.dat"))
         {
-            File.Delete(Application.persistentDataPath + "/MySaveData.dat"); 
-            
-            Debug.Log("Datareset complete!");
+            File.Delete(Application.persistentDataPath + "/MySaveData.dat");
+            Debug.Log("Data reset complete!");
         }
         else
-            Debug.LogError("No save data todelete.");
+        {
+            Debug.LogError("No save data to delete.");
+        }
     }
 
+    public static void SaveGame()
+    {
+        return;
+    }
 }
